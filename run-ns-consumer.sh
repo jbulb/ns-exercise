c760d7e46b41dfa735096769a643012bbe3a2811
Rody Cunningham <rody.cunningham@gmail.com>
2023-12-18 16:22:49 -0500

    update

---
+++ run-ns-consumer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo
+echo --- Pseudocode simulation ---
+echo This is a simulated script to run the consumer-side of the solution.
+echo Actual code would execute NSConsumer.scala on a cluster or master node, such as:
+echo
+echo ./run-on-cluster.sh nsconsumer.run
+echo
+
+
---
+++ run-ns-producer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo
+echo --- Pseudocode simulation ---
+echo This is a simulated script to run the produer-side of the solution.
+echo Actual code would execute NSProducer.scala on a cluster or master node, such as:
+echo
+echo ./run-on-cluster.sh nsproducer.run
+echo
+
+
